b0032f228cc5540c421c6b357b2a9bb6f6f7d90a
Jayanthi M <79845517+jayanthis952@users.noreply.github.com>
2023-02-22 22:48:28 +0530

    first commit

---
+++ src/views/Proposal.jsx
@@ -0,0 +1,13 @@
+
+import ProposalDetails from "../components/Proposal"
+import Voters from "../components/Voters"
+const proposal = () => {
+    return(
+        <>
+        <ProposalDetails />
+        <Voters />
+        </>
+    )
+}
+
+export default proposal
